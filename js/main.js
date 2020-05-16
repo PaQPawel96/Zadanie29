7c5a8e4be32a7f8ba4e13f3ca54e5598dbee44eb
PaQPaweł <paq-pawel1996@o2.pl>
2020-05-17 01:31:39 +0200

    Zadanie29

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let liczbaPierwsza=10;
+let liczbaDruga=30;
+function Suma(liczbaPierwsza, liczbaDruga)
+{
+ let wynik=liczbaPierwsza+liczbaDruga;
+
+    return wynik;
+}
+
+Suma(liczbaPierwsza, liczbaDruga);
